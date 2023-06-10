d276d99805c1f400d37c7919fd4f2ac6eca416b5
ChannelDanny <nanhquan1009@gmail.com>
2023-06-10 15:27:15 +0700

    Create Buoi_3.py

---
+++ Buổi 3/Buoi_3.py
@@ -0,0 +1,45 @@
+'''
+a = 10
+b = "string"
+print("a =", a, end = "\n")
+print("b =", b)
+print("a + b =", a ,b)
+print("a + b =", str(a) + " " + b, sep =" ")
+'''
+
+''' 
+print("Họ và tên:", input("Họ và tên: "))
+'''
+
+'''
+Ten = input("Tên của bạn: ")
+Ho_dem = input("Họ và tên đệm của bạn: ")
+Tuoi = input("Tuổi của bạn là: ")
+ 
+print("Họ và tên của bạn là: ", Ho_dem, Ten)
+print("Tuổi của bạn là: ", Tuoi)
+'''
+
+'''
+print("Người phỏng vấn, thú cưng trả lời")
+gender = input("Giới tính của bạn: ")
+food = input("Bạn thích ăn gì: ")
+hate = input("Thú cưng của bạn bị dị ứng cái gì: ")
+
+print("Giới tính của bạn: ", gender)
+print("Bạn thích ăn:" , food)
+print("Thứ cưng của bạn bị dị ứng:" , hate)
+'''
+
+'''
+a = int(input("a = " ))
+b = int(input("b = " ))
+c = int(input("c = " ))
+print("(a + b + c) : 3= ", (a + b + c)/3)2
+'''
+VND = float(3520500)
+USD = float(1500)
+c = float(1257125)
+print("1 đô bằng", VND / USD)
+print("số tháng là:", VND//c + 1)
+print("số tiền của tháng:", c * (VND//c + 1) / float (VND/USD))
