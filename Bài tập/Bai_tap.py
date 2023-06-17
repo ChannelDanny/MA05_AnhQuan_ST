bde89d450d8ffb6d405a44de83deebeb5904241f
ChannelDanny <nanhquan1009@gmail.com>
2023-06-17 15:31:42 +0700

    Create Bai_tap.py

---
+++ Bài tập/Bai_tap.py
@@ -0,0 +1,12 @@
+
+VND = float(input("Số tiền của bạn là:" ))
+print(float(VND * 0.000042),"USD")
+print(float(VND * 0.000033),"GBP")
+print(float(VND * 0.000039),"EUR")
+print(float(VND * 0.0060),"JPY")
+print(float(VND * 0.000013),"KWD")
+print(float(VND * 0.054),"KRW")
+print(float(VND * 0.0035),"RUB")
+
+
+
