d44043d57a69d89ce2465cefbd02e4087a91aafc
ChannelDanny <nanhquan1009@gmail.com>
2023-07-15 15:36:03 +0700

    Create Buoi_8.py

---
+++ Buổi 8/Buoi_8.py
@@ -0,0 +1,11 @@
+'''
+list_MA05 = ["phong", "Tuan", "Dong"]
+print(list_MA05[2])
+list_MA05[2]= "Long"
+print(list_MA05)
+print(len(list_MA05))
+'''
+
+A = [2,4,6,8,10,12]
+for j in range(len(A)):
+    print(A[j])
